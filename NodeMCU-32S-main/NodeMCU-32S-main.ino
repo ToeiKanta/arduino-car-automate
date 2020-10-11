07afc4f0632e98c869e1014dd8eda5325979f9ca
Kantapong Poonkasem <toei_kanta@hotmail.com>
2020-10-11 19:13:51 +0700

    add NodeMCU main

---
+++ NodeMCU-32S-main/NodeMCU-32S-main.ino
@@ -0,0 +1,49 @@
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = "YpzVX9uZtN0lnd6cnyWbFJAMC7bAihIe";
+char ssid[] = "Euangngam710";
+char pass[] = "enplace710";
+
+BlynkTimer timer;
+
+int pin = 2;
+
+void setup() { 
+  Serial1.begin(115200);
+  Serial1.println("NodeMCU connected...");
+  pinMode(pin, OUTPUT);
+  pinMode(pin, HIGH);
+  Serial.begin(9600);
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  int wifi_ctr = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ Serial.println("WiFi connected"); 
+ Blynk.begin(auth, ssid, pass);
+}
+
+BLYNK_CONNECTED(){
+  Blynk.syncVirtual(V1);
+}
+BLYNK_WRITE(V1){
+  int buttonState = param.asInt();
+  if (buttonState == 1) {
+         Serial.println("R01"); 
+         Serial1.print("R01");
+    }
+}
+
+void loop(){
+    Blynk.run();
+}
